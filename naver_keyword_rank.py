354da5d3c86aaa074376c43b4bf52430f822aafe
apotree <52978976+apotree@users.noreply.github.com>
2020-01-25 22:06:28 +0900

    Update naver_keyword_rank.py

--- naver_keyword_rank.py
+++ naver_keyword_rank.py
@@ -15,5 +15,5 @@ keyword_word = keyword.findAll("span", {"class": "ah_k"}) # 검색어 출력
 
 print("{}년 {}월 {}일 {}시 {}분 {}초 \n네이버 실시간 검색어\n".format(now.year, now.month, now.day, now.hour, now.minute, now.second))
 
-for i in range(0, 20):
+for i in range(0, 20): # 1위부터 20위까지 반복
     print(keyword_rank[i].text + "." + keyword_word[i].text)
